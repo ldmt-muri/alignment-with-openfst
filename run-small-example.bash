fa9c04cb7b94f417518a903bed1da28a21d9e995
Waleed Ammar <wammar@cs.cmu.edu>
2012-10-09 15:47:21 -0400

    replace run-example with two examples: one for a small-size corpus and another on a medium-size corpus

---
+++ run-small-example.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -l ncpus=1
+#PBS -l pmem=7gb
+#PBS -l walltime=10:00:00
+#PBS -d .
+#PBS -j oe
+#PBS -o /mal2/wammar/exp/tgt-rep/align/example/small.pbs.log
+
+#python utils/encode-corpus.py example/small.eng example/small.eng.vocab example/small.eng.int
+#python utils/encode-corpus.py example/small.kin example/small.kin.vocab example/small.kin.int
+make
+#./train-model1 example/small.eng.int example/small.kin.int example/small.out
+./train-loglinear example/small.eng.int example/small.kin.int example/small.out
+head example/small.out.param.final
