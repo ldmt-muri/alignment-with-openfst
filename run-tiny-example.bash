50811cba32a9d0bd266f29d1ed236c8cfb009afb
Waleed Ammar <wammar@cs.cmu.edu>
2012-10-24 12:41:56 -0400

    add a tiny dataset for debugging.

---
+++ run-tiny-example.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+#PBS -l ncpus=1
+#PBS -l pmem=7gb
+#PBS -l walltime=10:00:00
+#PBS -d .
+#PBS -j oe
+#PBS -o /mal2/wammar/exp/tgt-rep/align/example/small.pbs.log
+
+python utils/encode-corpus.py example/tiny.eng example/tiny.eng.vocab example/tiny.eng.int
+python utils/encode-corpus.py example/tiny.kin example/tiny.kin.vocab example/tiny.kin.int
+make
+./train-loglinear example/tiny.eng.int example/tiny.kin.int example/small.out
