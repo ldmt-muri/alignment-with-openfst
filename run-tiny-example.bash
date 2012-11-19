ed6d18b16e9c42ed36209a5b812141ce28225e2c
Waleed Ammar <wammar@allegro.clab.cs.cmu.edu>
2012-11-19 11:18:11 -0500

    update the tiny example with a test set

--- run-tiny-example.bash
+++ run-tiny-example.bash
@@ -6,8 +6,10 @@
 #PBS -j oe
 #PBS -o /mal2/wammar/exp/tgt-rep/align/example/small.pbs.log
 
-python utils/encode-corpus.py example/tiny.eng example/tiny.eng.vocab example/tiny.eng.int
-python utils/encode-corpus.py example/tiny.kin example/tiny.kin.vocab example/tiny.kin.int
+python utils/trie-encode-corpus.py example/tiny.eng example/tiny.eng.vocab example/tiny.eng.int
+python utils/trie-encode-corpus.py example/tiny.kin example/tiny.kin.vocab example/tiny.kin.int
+python utils/trie-encode-corpus.py example/tiny.eng-test example/tiny.eng.vocab example/tiny.eng-test.int ready
+python utils/trie-encode-corpus.py example/tiny.kin-test example/tiny.kin.vocab example/tiny.kin-test.int ready
 make
-./train-loglinear example/tiny.eng.int example/tiny.kin.int example/tiny.eng.vocab example/tiny.kin.vocab example/tiny.out
+./train-loglinear example/tiny.eng.int example/tiny.kin.int example/tiny.eng-test.int example/tiny.kin-test.int example/tiny.eng.vocab example/tiny.kin.vocab example/tiny.out
 #./train-hmm example/tiny.eng.int example/tiny.kin.int example/tiny.out
